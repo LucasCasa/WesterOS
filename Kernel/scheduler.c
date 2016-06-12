de23f11f2082af3e2eb7491064fdd60b9eeea6f2
Lucas Casa <lcasagrande@itba.edu.ar>
2016-06-12 15:43:53 -0300

    ahora el juego arranca invulnerables, tiene 4 jugadores(aunque el 4to no se puede manejar) y arrancan en posiciones random

---
+++ Kernel/scheduler.c
@@ -0,0 +1,32 @@
+#include "scheduler.h"
+#include "process.h"
+
+
+
+void init_scheduler(){
+
+
+}
+
+void next_process(){
+
+
+}
+
+void add_new_process(){
+
+
+}
+
+void remove_process(uint8_t pid){
+
+
+}
+
+void push_to_top(uint8_t pid){
+
+}
+void get_all_process(){
+
+
+}
