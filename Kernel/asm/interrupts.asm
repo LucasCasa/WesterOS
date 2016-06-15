e31ff2c2f7711baac83c499db93688b5b024b5b5
LucasCasa <lcasagrande@itba.edu.ar>
2016-06-15 12:09:30 -0300

    tratando de hacer el screensaver

--- Kernel/asm/interrupts.asm
+++ Kernel/asm/interrupts.asm
@@ -22,6 +22,45 @@ EXTERN keyboard_handler
 EXTERN sys_manager
 EXTERN piano_handler
 
+%macro	pushState 0
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rbp
+	push rdi
+	push rsi
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+	push fs
+	push gs
+%endmacro
+
+%macro	popState 0
+	pop gs
+	pop fs
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rsi
+	pop rdi
+	pop rbp
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+%endmacro
 
 
 SECTION .text
@@ -38,12 +77,12 @@ _sti:
 _write_port:
     push rbp
     mov rbp, rsp
-    mov edx, edi    
+    mov edx, edi
     mov rax, rsi
-    and rax, 0FFFFh  
+    and rax, 0FFFFh
     out dx, al
     mov rsp,rbp
-    pop rbp  
+    pop rbp
     ret
 
 picMasterMask:
@@ -66,40 +105,36 @@ _int80_hand:
     call sys_manager
     iretq
 
-_int_timer_hand:				; Handler de INT 8 ( Timer tick)
-   	push rdi
-	push rax                  ; Se salvan los registros
+_int_timer_hand:
+		pushState; Handler de INT 8 ( Timer tick) ; Se salvan los registros
                             ; Carga de DS y ES con el valor del selector
     mov     ax, 10h			; a utilizar.
     mov     ds, ax
     mov     es, ax
    	call    timer_handler
-    
+
     mov		al,20h			; Envio de EOI generico al PIC
 	out		20h,al
-	
-	pop 	rax
-	pop 	rdi
+
+		popState
     iretq
 
 
 _int_keyboard_hand:				; Handler de INT 9 ( Teclado )
-    push   	rdi
-    push   	rax                      ; Se salvan los registros
-                          
+    pushState                      ; Se salvan los registros
+
     xor     rax,rax
 	in 		al, 60h	              ;Leo el puerto del teclado
 	and     rax,0x00000000000000FF
     xor     rdi,rdi
     mov     rdi,rax	            ;Le envio el SCAN CODE como parametro a la funcion int_09
 	call 	keyboard_handler	;Llamo a la interrupcion que maneja el SCAN CODE en C
-	
+
 
     mov		al,20h			; Envio de EOI generico al PIC
 	out		20h,al
 
-    pop     rax
-    pop     rdi
+    popState
     iretq
 
 _call_int80:
@@ -117,30 +152,30 @@ haltcpu:
 
 
 _int_start_sound:
-		
+
         mov     al, 182         ; Prepare the speaker for the
         out     43h, al         ;  note.
 		xor 	rax, rax
         mov     rax, rdi        ; Frequency number (in decimal)
         out     42h, al         ; Output low byte.
         mov     al, ah          ; Output high byte.
-        out     42h, al 
+        out     42h, al
         in      al, 61h         ; Turn on note (get value from
                                 ;  port 61h).
         or      al, 00000011b   ; Set bits 1 and 0.
         out     61h, al         ; Send new value.
         ; Send new value.
-	ret	
-			
+	ret
+
 _beep:
-        
+
         mov     al, 182         ; Prepare the speaker for the
         out     43h, al         ;  note.
         mov     rax, 4560        ; Frequency number (in decimal)
                                 ;  for middle C.
         out     42h, al         ; Output low byte.
         mov     al, ah          ; Output high byte.
-        out     42h, al 
+        out     42h, al
         in      al, 61h         ; Turn on note (get value from
                                 ;  port 61h).
         or      al, 00000011b   ; Set bits 1 and 0.
@@ -157,17 +192,17 @@ _beep:
                                 ;  port 61h).
         and     al, 11111100b   ; Reset bits 1 and 0.
         out     61h, al         ; Send new value.
-    ret 
+    ret
 
 _song_note:
-       
+
         mov     al, 182         ; Prepare the speaker for the
         out     43h, al         ;  note.
 		xor		rax, rax
         mov     rax, rdi        ; Frequency number (in decimal)
         out     42h, al         ; Output low byte.
         mov     al, ah          ; Output high byte.
-        out     42h, al 
+        out     42h, al
         in      al, 61h         ; Turn on note (get value from
 	                                ;  port 61h).
         or      al, 00000011b   ; Set bits 1 and 0.
@@ -184,7 +219,7 @@ _song_note:
 	                                ;  port 61h).
         and     al, 11111100b   ; Reset bits 1 and 0.
         out     61h, al         ; Send new value.
-	    ret 
+	    ret
 
 
 _int_end_sound:
@@ -193,5 +228,3 @@ _int_end_sound:
  	and al, -4
  	out 61h, al
  	ret
-	
-	
--- Kernel/include/screensaver.h
+++ Kernel/include/screensaver.h
@@ -11,11 +11,11 @@
 void alien();
 void show_screensaver();
 
-typedef struct TarImage{
+typedef struct ScreenImage{
   unsigned int 	 width;
   unsigned int 	 height;
   unsigned int 	 bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */
-  unsigned char	 pixel_data[200 * 201 * 3 + 1];
-} TarImage;
+  unsigned char	 pixel_data[105 * 105 * 3 + 1];
+} ScreenImage;
 
 #endif
